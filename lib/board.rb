f6b02ed9ccc64bf172e8813710ec465ee74a735d
ichanmsngkyy <128625300+ichanmsngkyy@users.noreply.github.com>
2025-06-04 05:08:40 +0800

    Implement King movement and castling in board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,147 @@
 # frozen_string_literal:true
 
+require_relative 'pieces/rook'
+require_relative 'pieces/bishop'
+require_relative 'pieces/knight'
+require_relative 'pieces/king'
+require_relative 'pieces/queen'
+require_relative 'pieces/pawn'
+require_relative 'pieces/piece'
+
 # Board Class
 class Board
   attr_reader :grid
 
   def initialize
     @grid = (Array.new(8) { Array.new(8) })
+    setup_piece
   end
 
   def setup_piece
     white_pieces
     black_pieces
+  end
+
+  def white_pieces
+    @grid[0][0] = Rook.new([0, 0], true)
+    @grid[0][1] = Bishop.new([0, 1], true)
+    @grid[0][2] = Knight.new([0, 2], true)
+    @grid[0][3] = Queen.new([0, 3], true)
+    @grid[0][4] = King.new([0, 4], true)
+    @grid[0][5] = Knight.new([0, 5], true)
+    @grid[0][6] = Bishop.new([0, 6], true)
+    @grid[0][7] = Rook.new([0, 7], true)
+    (0..7).each do |col|
+      @grid[1][col] = Pawn.new([1, col], true)
+    end
+  end
+
+  def black_pieces
+    @grid[7][0] = Rook.new([7, 0], false)
+    @grid[7][1] = Bishop.new([7, 1], false)
+    @grid[7][2] = Knight.new([7, 2], false)
+    @grid[7][3] = Queen.new([7, 3], false)
+    @grid[7][4] = King.new([7, 4], false)
+    @grid[7][5] = Knight.new([7, 5], false)
+    @grid[7][6] = Bishop.new([7, 6], false)
+    @grid[7][7] = Rook.new([7, 7], false)
+
+    (0..7).each do |col|
+      @grid[6][col] = Pawn.new([6, col], false)
+    end
+  end
+
+  def [](position)
+    row, col = position
+    @grid[row][col]
+  end
+
+  def []=(position, piece)
+    row, col = position
+    piece[row][col]
+  end
+
+  def move_piece(start_pos, end_pos)
+    piece = [start_pos][end_pos]
+
+    return false if piece.nil?
+    return false unless piece.valid_move?(end_pos, self)
+
+    self[start_pos] = nil
+    self[end_pos] = piece
+    piece.position = piece
+    piece.has_moved!
+
+    true
+  end
+
+  def can_castle?(color, side)
+    king_position = find_king(color)
+    rook_position = find_rook(color, side)
+
+    return false if king_position.nil?
+    return false if rook_position.nil?
+
+    king = self[king_position]
+    rook = self[rook_position]
+
+    return false if king.has_moved || rook.has_moved
+
+    square_between = get_castle_path(color, side)
+    squares_between.each do |square|
+      return false unless self[square].nil?
+    end
+
+    return false if in_check?(color)
+
+    path_squares = get_king_castle_path(color, side) # You need this helper
+    path_squares.each do |square|
+      return false if square_under_attack?(square, opponent_color)
+    end
+    false if square_under_attack?(final_king_position, opponent_color)
+  end
+
+  private
+
+  def find_king(color)
+    (0..7).each do |row|
+      (0..7).each do |col|
+        return [row, col] if piece && piece.is_a?(King) && piece.color == color
+      end
+    end
+    nil
+  end
+
+  def find_rook(color, side)
+    (0..7).each do |row|
+      (0..7).each do |col|
+        return [row, col] if piece && piece.is_a?(Rook) && piece.color == color
+      end
+    end
+    nil
+  end
+
+  def in_check?(color)
+    king_position = find_king(color)
+    return false if king_position.nil?
+
+    opponent_color = color == 'white' ? 'black' : 'white'
+    square_under_attack?(king_position, opponent_color)
+  end
+
+  def square_under_attack?(position, color)
+    (0..7).each do |row|
+      (0..7).each do |col|
+        piece = @grid[row][col]
+
+        next if piece.nil || piece.color != color
+
+        return true if piece.valid_move?(position, self)
+      end
+    end
+    false
+  end
+
+  def get_castle_path(color, side)
   end
 end
--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -4,4 +4,28 @@ require_relative 'piece'
 
 # King movement Class
 class King < Piece
+  attr_reader :position, :icon
+
+  def initialize(position, is_white)
+    @icon = is_white ? '♔' : '♚'
+    super(position, is_white, @icon)
+  end
+
+  def valid_move?(end_pos, board)
+    return false unless on_board?(end_pos)
+    return false unless king_move?(end_pos)
+    return false unless clear_path?(end_pos, board)
+
+    destination_valid?(end_pos, board)
+  end
+
+  private
+
+  # King movement
+  def king_move?(end_pos)
+    dx = (end_pos[0] - position[0]).abs
+    dy = (end_pos[1] - position[1]).abs
+
+    dx <= 1 && dy <= 1 && dx != 0 && dy != 0
+  end
 end
